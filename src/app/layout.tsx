e74dc09f9b6283671a72c2139dd3dccd775b55e1
Arnav <105158586+arnavbee@users.noreply.github.com>
2024-05-24 21:41:20 +0530

    main

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,17 +6,25 @@ import "./globals.css";
 import { ThirdwebProvider, ChainId } from "@thirdweb-dev/react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Navbar } from "../../components/Navbar";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Home from "./page";
+
 
 const activeChain = "sepolia";
+const queryClient = new QueryClient();
+
 
 export default function MyApp() {
   return (
     <html>
       <body>
-      <ThirdwebProvider activeChain={activeChain}>
+      <ThirdwebProvider activeChain={activeChain} clientId="ec8f9fa6a4005c62b10ccb4e78b211d6">
+      <QueryClientProvider client={queryClient}>
       <ChakraProvider>
         <Navbar />
+        <Home/>
       </ChakraProvider>
+      </QueryClientProvider>
     </ThirdwebProvider>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,20 @@ import Image from "next/image";
 import { ConnectButton } from "thirdweb/react";
 import thirdwebIcon from "@public/thirdweb.svg";
 import { client } from "./client";
-import {Container, Heading} from "@chakra-ui/react"
+import {Container, Heading, Flex, Stack, Button} from "@chakra-ui/react"
+import NextLink from "next/link";
 
 
 export default function Home(){
   return (
-    <Container>
-      <Heading>
-        Marketplace
-      </Heading>
+    <Container maxW={"1200px"}>
+     <Flex h={"80vh"} alignItems={"center"} justifyContent={"center"}>
+     <Stack spacing={4} align={"center"}>
+      <Heading>Dead NFTs Marketplace</Heading>
+      <h3>A Rebirth of Forgotten Digital Treasures</h3>
+      <Button as={NextLink} href="/buy">Shop NFTs</Button>
+     </Stack>
+     </Flex>
     </Container>
   )
 }
